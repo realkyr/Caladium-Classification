626e011f7cda4a6c8d58509dd11621dde4b8a94d
obonetree <70530586+obonetree@users.noreply.github.com>
2021-05-23 19:15:13 +0700

    Add files via upload

---
+++ learning_based.py
@@ -0,0 +1,74 @@
+#import library
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# funtion onehot example have 3 classes 1 = [1,0,0] , 2 = [0,1,0] , 3 = [0,0,1]
+def onehot(Y, nclass=12):
+  Y_ = np.zeros((Y.shape[0], nclass))
+  for i, y in enumerate(Y):
+    Y_[i, Y[i]] = 1
+  return Y_
+
+# import data
+train = pd.read_csv('C:/Users/User/Desktop/Learning_based/train.csv')
+test = pd.read_csv('C:/Users/User/Desktop/Learning_based/test.csv')
+train_x = train.iloc[:,3:-2]
+test_x = test.iloc[:,3:-2]
+train_y = train['labels']
+train_y = onehot(train_y)
+test_y = test['labels']
+test_y = onehot(test_y)
+
+# create model
+import tensorflow as tf
+d_in = (train_x.shape[1],)
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(260, input_shape=d_in,activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(520, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(520, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(260, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(130, activation='relu'))
+model.add(tf.keras.layers.Dense(12, activation='softmax'))
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                        optimizer=tf.keras.optimizers.RMSprop(
+    learning_rate=0.001,
+    rho=0.9,
+    momentum=0.0,
+    epsilon=1e-07,
+    centered=False,
+    name="RMSprop"),metrics=['accuracy'])
+
+# train model and save best model
+from keras.callbacks import ModelCheckpoint
+train_x = np.array(train_x)
+test_x = np.array(test_x)
+train_y = np.array(train_y)
+test_y = np.array(test_y)
+checkpoint = ModelCheckpoint('C:/Users/User/Desktop/Learning_based/best_model.h5', verbose=1, monitor='val_accuracy',save_best_only=True, mode='auto') 
+history = model.fit(train_x, train_y, epochs=1000, validation_data=(test_x,test_y), callbacks=[checkpoint])
+
+# evaluate the model
+train_acc = model.evaluate(train_x, train_y, verbose=0)
+test_acc = model.evaluate(test_x, test_y, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc[1]*100, test_acc[1]*100))
+
+# plot loss during training
+plt.subplot(211)
+plt.title('Loss')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+
+# plot accuracy during training
+plt.subplot(212)
+plt.title('Accuracy')
+plt.plot(history.history['accuracy'], label='train')
+plt.plot(history.history['val_accuracy'], label='test')
+plt.legend()
+plt.show()
---
+++ learning_based_test.py
@@ -0,0 +1,72 @@
+#import library
+from tensorflow import keras
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# import best model
+model = keras.models.load_model('C:/Users/User/Desktop/Learning_based/best_model.h5')
+
+# funtion onehot example have 3 classes 1 = [1,0,0] , 2 = [0,1,0] , 3 = [0,0,1]
+def onehot(Y, nclass=12):
+  Y_ = np.zeros((Y.shape[0], nclass))
+  for i, y in enumerate(Y):
+    Y_[i, Y[i]] = 1
+  return Y_
+
+#import test data
+test = pd.read_csv('C:/Users/User/Desktop/Learning_based/test.csv')
+test_x = test.iloc[:,3:-2]
+test_y = test['labels']
+test_y = onehot(test_y)
+test_x = np.array(test_x)
+test_y = np.array(test_y)
+
+# test model
+Z = model.predict(test_x)
+result = Z
+for i in range(len(result)):
+  for j in range(len(result[i])):
+    if result[i][j] == max(result[i]):
+      result[i][j] = 1
+    else:
+      result[i][j] = 0
+accuracy = model.evaluate(test_x, test_y)
+print(accuracy[1]*100)
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+import sklearn
+tranform = []
+for i in range(len(result)):
+  for j in range(len(result[i])):
+    if result[i][j] == 1:
+      tranform.append(j)
+y_true = test['labels'].tolist()
+y_pred = tranform
+CM = confusion_matrix(y_true, y_pred)
+
+# precision , recall , f1 score and accuracy score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('\nAccuracy: {:.2f}\n'.format(accuracy_score(y_true, y_pred)))
+
+print('Micro Precision: {:.2f}'.format(precision_score(y_true, y_pred, average='micro')))
+print('Micro Recall: {:.2f}'.format(recall_score(y_true, y_pred, average='micro')))
+print('Micro F1-score: {:.2f}\n'.format(f1_score(y_true, y_pred, average='micro')))
+
+print('Macro Precision: {:.2f}'.format(precision_score(y_true, y_pred, average='macro')))
+print('Macro Recall: {:.2f}'.format(recall_score(y_true, y_pred, average='macro')))
+print('Macro F1-score: {:.2f}\n'.format(f1_score(y_true, y_pred, average='macro')))
+
+print('Weighted Precision: {:.2f}'.format(precision_score(y_true, y_pred, average='weighted')))
+print('Weighted Recall: {:.2f}'.format(recall_score(y_true, y_pred, average='weighted')))
+print('Weighted F1-score: {:.2f}'.format(f1_score(y_true, y_pred, average='weighted')))
+
+from sklearn.metrics import classification_report
+print('\nClassification Report\n')
+print(classification_report(y_true, y_pred, target_names=['Class 1', 'Class 2', 'Class 3','Class 4',
+                                                          'Class 5','Class 6','Class 7','Class 8',
+                                                          'Class 9','Class 10','Class 11','Class 12']))
